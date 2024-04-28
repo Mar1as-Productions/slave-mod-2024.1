7fbcfe09146dd5697170c55cb47117fb89ac27c2
Krisp1k <vladadoskar@gmail.com>
2024-04-28 12:11:35 +0200

    hut

--- src/main/java/mp/slave_mod/procedures/KKKtotemAdditionalGenerationConditionProcedure.java
+++ src/main/java/mp/slave_mod/procedures/KKKtotemAdditionalGenerationConditionProcedure.java
@@ -18,7 +18,7 @@ public class KKKtotemAdditionalGenerationConditionProcedure {
 						|| (world.getBlockState(BlockPos.containing(x, y - 1, z - 1))).getBlock() == Blocks.GRASS_BLOCK && (world.getBlockState(BlockPos.containing(x, y - 1, z - 2))).getBlock() == Blocks.GRASS_BLOCK
 								&& (world.getBlockState(BlockPos.containing(x, y - 1, z + 1))).getBlock() == Blocks.GRASS_BLOCK && (world.getBlockState(BlockPos.containing(x, y - 1, z + 2))).getBlock() == Blocks.GRASS_BLOCK)) {
 			if (world instanceof ServerLevel _serverworld) {
-				StructureTemplate template = _serverworld.getStructureManager().getOrCreate(new ResourceLocation("slave_mod", "kkkkriz1.1"));
+				StructureTemplate template = _serverworld.getStructureManager().getOrCreate(new ResourceLocation("slave_mod", "kkkkriz10"));
 				if (template != null) {
 					template.placeInWorld(_serverworld, BlockPos.containing(x, y, z), BlockPos.containing(x, y, z), new StructurePlaceSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setIgnoreEntities(false), _serverworld.random, 3);
 				}
